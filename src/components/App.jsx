3afb1a3fe4620dc918a089732b24bef970a3db61
KsiRT <130671776+KsiRT@users.noreply.github.com>
2023-10-13 15:19:26 +0300

    result no styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,19 +6,26 @@ import { ContactList } from './Contactlist/ContactList';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
     name: '',
     number: '',
   };
+
   // Принимаем фильтр из инпута
   handleFilterInput = value => {
     this.setState({ filter: value });
+  };
+  addNewContact = newContact => {
+    const { contacts } = this.state;
+    const checkContact = contacts.find(
+      contact => contact.name === newContact.name
+    );
+    checkContact
+      ? Notiflix.Notify.failure(`${newContact.name} is already in contacts`)
+      : this.setState(prev => ({
+          contacts: [...prev.contacts, newContact],
+        }));
   };
   // Фильтруем
   getFilteredContact = () => {
@@ -32,7 +39,9 @@ export class App extends Component {
   };
   // Удаляем контакт
   handleDelete = (id, name) => {
-    this.setState(prev => prev.contacts.filter(contact => contact.id !== id));
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
 
     Notiflix.Notify.success(
       `${name} was successfully deleted from your Phonebook`
@@ -45,7 +54,7 @@ export class App extends Component {
     return (
       <>
         <h1>Phonebook</h1>
-        <ContactForm />
+        <ContactForm newContact={this.addNewContact} />
 
         <h2>Contacts</h2>
         <Filter filter={filter} onFilterInput={this.handleFilterInput} />
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,23 +1,63 @@
 import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
 
 export default class ContactForm extends Component {
+  state = { name: '', number: '' };
+
+  // вводимо в інпут
+  hendleAddInput = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  //додаємо до списку
+  hendleFormSubmit = e => {
+    e.preventDefault();
+
+    const { name, number } = this.state;
+    if (name.trim() === '' || number.trim() === '') {
+      return;
+    }
+
+    const newContact = { id: nanoid(), name, number };
+    this.setState({ name: '', number: '' });
+    this.props.newContact(newContact);
+  };
+
   render() {
+    const { name, number } = this.state;
     return (
-      <>
-        <form>
-          <label>
-            <p>Name</p>
-            <input type="text" name="Name" />
-          </label>
-
-          <label>
-            <p>Number</p>
-            <input type="text" name="Number" />
-          </label>
-
-          <button type="submit">Add contact</button>
-        </form>
-      </>
+      <form onSubmit={this.hendleFormSubmit}>
+        <label htmlFor="name">
+          Name
+          <input
+            value={name}
+            id="name"
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            onChange={this.hendleAddInput}
+            placeholder="Name Surname"
+          />
+        </label>
+        <label htmlFor="name">
+          Number
+          <input
+            value={number}
+            id="name"
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            onChange={this.hendleAddInput}
+            placeholder="Phone Number"
+          />
+        </label>
+        <button>Add contact</button>
+      </form>
     );
   }
 }
